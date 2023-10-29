153b547497208da0b42fca9136aa7251f1f45bdf
Ryan Bagan <darcknight1980@gmail.com>
2023-10-28 23:21:31 -0700

    added daisyUI navbar

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li><a>Link</a></li>
+              <li>
+                <details>
+                  <summary>
+                    Parent
+                  </summary>
+                  <ul className="p-2 bg-base-100">
+                    <li><a>Link 1</a></li>
+                    <li><a>Link 2</a></li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
