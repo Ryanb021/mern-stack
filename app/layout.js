c492389d6b676458f884ee322ec725971fa08134
Ryan Bagan <darcknight1980@gmail.com>
2023-10-28 23:30:02 -0700

    added daisyUI navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,15 +22,7 @@ export default function RootLayout({ children }) {
             <ul className="menu menu-horizontal px-1">
               <li><a>Link</a></li>
               <li>
-                <details>
-                  <summary>
-                    Parent
-                  </summary>
-                  <ul className="p-2 bg-base-100">
-                    <li><a>Link 1</a></li>
-                    <li><a>Link 2</a></li>
-                  </ul>
-                </details>
+                
               </li>
             </ul>
           </div>
